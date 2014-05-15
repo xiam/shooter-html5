6f0f4a5a2ef3368abd32d4193b57e026bb10834b
José Carlos Nieto <jose.carlos@menteslibres.net>
2014-05-15 10:26:02 -0500

    Dynamic hostname.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // Websocker server address.
-var WEBSOCKET_SERVICE = 'ws://shooter.io/w/';
+var WEBSOCKET_SERVICE = 'ws://' + location.hostname + ':3223/w/';
 
 // Frames configuration.
 var FRAMES_PER_SECOND = 24;
